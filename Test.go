e13bd87526b4fb33f015285c881669f7ba0627ea
azmi17 <azmifarhan80@gmail.com>
2021-11-30 20:09:23 +0700

    minor change

--- Test.go
+++ Test.go
@@ -1,5 +1,5 @@
 package Test
 
-func SayHello(name string) string {
-	return "Hello " + name
+func SayHello(name string, age int) string {
+	return "Hello my name is " + name
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/azmi17/testing-go-modules/V2
+module github.com/azmi17/testing-go-modules/v2
 
 go 1.17
